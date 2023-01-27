e1dab90f9b7dace523256bc45fb35f795cf5bdac
MathiasFar <mhsvarre@gmail.com>
2023-01-27 19:07:39 +0100

    Updated to demonstrate the headers functionality

--- example.cpp
+++ example.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include <string>
 #include "base64.h"
 
 using namespace std;
 
 int main() {
-    cout<<b64_encode("test123")<<"\n";
+    string example_string{"test phrase"};
+    string x = b64_encode(example_string);
+    cout<<"Base64 Encoded: "<<x<<"\n";
+    cout<<"Base64 Decoded: "<<b64_decode(x)<<"\n";
     return 0;
 }
