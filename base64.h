1d28b7d737eeb60ba4fcbde3f3a9bca4fc465b90
MathiasFar <mhsvarre@gmail.com>
2023-01-27 19:07:05 +0100

    Implemented decode function

--- base64.h
+++ base64.h
@@ -2,8 +2,8 @@
 #include <algorithm>
 #include <string>
 #include <vector>
-const char b64Alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-std::string b64_encode(std::string input){
+std::string b64Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+std::string b64_encode(std::string input) {
     // converts input to 8 bit bytes and stores it in buffer
     std::string buffer;
     std::string output;
@@ -13,7 +13,7 @@ std::string b64_encode(std::string input){
     }
     // pads buffer so its len%6==0
     int rem=6-(buffer.length()%6);
-    std::vector<std::string>groups;
+    std::vector<std::string>bytes;
     for(int i=0;i<=rem;++i) {
         buffer+="0";
     }
@@ -23,15 +23,41 @@ std::string b64_encode(std::string input){
         for(int j=0;j<6;++j) {
             element+=buffer[i+j];
         }
-        groups.push_back("00"+element);
+        bytes.push_back("00"+element);
     }
     // converts the 6 bit bytes to ascii by using its dec value as an index in b64table
-    for(auto &e:groups) {
-        output+=b64Alphabet[strtol(e.c_str(), nullptr, 2)];
+    for(auto &e:bytes) {
+        output+=b64Alphabet[strtol(e.c_str(),nullptr,2)];
     }
     for(int i=0;i< 3-(input.length()%3);++i) {
         if(input.length()%3!=0)
             output+="=";
+    }
+    return output;
+}
+std::string b64_decode(std::string input) {
+    std::string buffer;
+    std::string output;
+    // convert base64 char to binary and then concat onto buffer
+    for(auto &ch:input) {
+        for(int i=0;i<b64Alphabet.length();++i) {
+            if((char)b64Alphabet[i]==ch) {
+                std::bitset<6>byte(i);
+                buffer+=byte.to_string();
+            }
+        }
+    }
+    // splits the buffer into 8 bit bytes and discards the remainding
+    std::vector<std::string>bytes;
+    for(int i=0;i<buffer.length()-1;i+=8) {
+        std::string element;
+        for(int j=0;j<8;++j) {
+            element+=buffer[i+j];
+        }
+        bytes.push_back(element);
+    }
+    for(auto &e:bytes) {
+        output+=(char)strtol(e.c_str(),nullptr,2);
     }
     return output;
 }
